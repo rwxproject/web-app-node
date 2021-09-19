50c7418aeb08672c252730c01b81a134abd8b2ff
rwxproject <rwxproject@gmail.com>
2021-09-19 09:55:36 -0400

    headers update

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
-const os = require('os');
+const os = require("os");
 const host = os.hostname();
 
-const port = process.env.PORT || '3000';
-const serverAddress = process.env.SRVADDR || 'localhost';
-const serverPort = process.env.SRVPORT || '3000';
-const apiTimer = process.env.APITIME || '5000';
+const port = process.env.PORT || "3000";
+const serverAddress = process.env.SRVADDR || "localhost";
+const serverPort = process.env.SRVPORT || "3000";
+const apiTimer = process.env.APITIME || "5000";
 
-app.get('/', (req, res) => res.send(`${host}`))
-app.get('/api', (req, res) => {
-    const time = Date.now();
-    res.json({host, time});
+app.get("/", (req, res) => res.send(`${host}`));
+app.get("/api", (req, res) => {
+  const userId = req.header("x-user-id") || "user";
+  const time = Date.now();
+  res.json({ host, time, userId });
 });
 
-setInterval(()=> {
-    axios.get(`http://${serverAddress}:${serverPort}/api`)
-    .then(function(response) {
-    // console.log(response.headers);
-    console.log(response.data);
-  });
-},apiTimer)
+setInterval(() => {
+  axios
+    .get(`http://${serverAddress}:${serverPort}/api`, {
+      headers: { "x-user-id": host },
+    })
+    .then(function (response) {
+      console.log(response.headers);
+      console.log(response.data);
+    });
+}, apiTimer);
 
-app.listen(port, () => console.log(`${host} Listening on port ${port}`))
+app.listen(port, () => console.log(`${host} Listening on port ${port}`));
